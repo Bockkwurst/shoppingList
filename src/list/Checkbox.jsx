a755c1b894f773475b8a0f56e02d2be7db3a7503
Bockkwurst <147827734+Bockkwurst@users.noreply.github.com>
2024-03-13 15:25:12 +0100

    a

---
+++ src/list/Checkbox.jsx
@@ -0,0 +1,15 @@
+import Styles from "./List.module.css"
+function Checkbox({name, checked, onChange}){
+    return(
+        <div className="checkbox">
+            <input
+                type="checkbox"
+                name={name}
+                checked={checked}
+                onChange={onChange}
+                />
+            <label htmlFor={name}>{name}</label>
+        </div>
+    );
+}
+export default Checkbox
